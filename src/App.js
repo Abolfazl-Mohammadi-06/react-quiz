6505a54a10637bf50bdb32677e258225d2a0d640
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-06-30 23:34:15 +0330

    starting the react quiz project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import DateCounter from './DateCounter';
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+    questions: [],
+//
+    status:'loading',
+}
+
+function reducer(state, action) {
+
+}
 
 export default function App() {
-  return <div>
-      <DateCounter/>
-  </div>
+    const [state, dispatch] = useReducer(reducer, initialState);
+    useEffect(function () {
+        fetch("http://localhost:8000/questions").then(res => res.json()).then(data => {
+            console.log(data)
+        }).catch(err => console.error("Error"));
+    }, [])
+
+    return <div className='app'>
+        <Header/>
+        <Main>
+            <p>1/15</p>
+            <p>Question?</p>
+        </Main>
+    </div>
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -4,10 +4,6 @@ const initialState = {count: 0, step: 1};
 
 function reducer(state, action) {
     console.log(state);
-    // if (action.type === "dec") return state - action.payload;
-    // if (action.type === "inc") return state + action.payload;
-    // if (action.type === "setCount") return action.payload;
-
 
     switch (action.type) {
         case "dec":
